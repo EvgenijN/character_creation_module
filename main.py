4f664414a58b196bb0eb7530108564c49fe781a0
Евгений <satis-irk@mail.ru>
2023-10-05 21:22:02 +0500

    add working version without classes

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
                 f'{5 + randint(-3, -1)}')
     return
 
+
 def defence(char_name: str, char_class: str) -> str:
     """Calculate the efficiency of protection."""
     if char_class == 'warrior':
@@ -27,7 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
-    """Special skils."""
+    """Apple special skill."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -39,7 +40,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
-    """Enter name, select character"""
+    """Start training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -64,7 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    """Select player"""
+    """Select player."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
